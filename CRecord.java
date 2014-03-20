991009eaed963f1ee43d8c72a9e4a81921c43638
axiel92 <axielito_92-07@live.com.mx>
2014-03-20 09:41:15 -0600

    Update CRecord.java

--- CRecord.java
+++ CRecord.java
@@ -6,7 +6,13 @@ private Double peso;
 private Double estatura;
 
 /** Implementar constructores **/
-
+public CRecord(string c, string n, string a, Double p, Double p, Double e){
+clave = c;
+nombre = n;
+apellido = a;
+peso = p;
+estatura =e;
+}
 /** Implementar setters **/
 
 /** Implementar getters **/
